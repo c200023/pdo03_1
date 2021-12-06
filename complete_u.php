d56064d99e7a39c9dc459e1e06b9d05a040cdeaa
c200023 <83998980+c200023@users.noreply.github.com>
2021-12-06 16:22:19 +0900

    Update complete_u.php

--- complete_u.php
+++ complete_u.php
@@ -3,15 +3,15 @@
   require_once("functions.php");
 
   /* (1)ここにセッション変数の受け渡し処理を追加して下さい  */
-
-
-
-
+  $name = $_SESSION["name"];
+  $email = $_SESSION["email"];
+  $gender = $_SESSION["gender"];
+  $edit = $_SESSION["edit"];
 
 $dbh = db_conn();      // データベース接続
 try{
     /* (2)ここにSQL文＋プレースホルダー　の文字列を準備する処理を追加して下さい  */
-
+    $sql = "SELECT :name, :email, :gender FROM user WHERE id = :id";
     $stmt = $dbh->prepare($sql);                           //クエリの実行準備
     $stmt->bindValue(':email', $email, PDO::PARAM_STR);    //バインド:プレースホルダ―の値を埋める
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);      //バインド:プレースホルダ―の値を埋める
